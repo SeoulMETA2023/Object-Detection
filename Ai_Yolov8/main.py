6e5d55b01cd654d1306582abea1f9823e2882e60
Jihoon1218 <milky1892@icloud.com>
2023-07-14 12:42:04 +0900

    yolov8_1

--- Ai_Yolov8/main.py
+++ Ai_Yolov8/main.py
@@ -1,41 +1,12 @@
-import cv2
 from ultralytics import YOLO
-import numpy as np
-
-import torch
-
-cap = cv2.VideoCapture(0)
+from PIL import Image
+import cv2
 
 model = YOLO("yolov8n.pt")
+# accepts all formats - image/dir/Path/URL/video/PIL/ndarray. 0 for webcam
+# from ndarray
+im2 = cv2.imread("image.jpg")
+results = model.predict(source=im2, save=True)  # save predictions as labels
 
-w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = cap.get(cv2.CAP_PROP_FPS)
-
-fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-delay = round(1000/fps)
-
-out = cv2.VideoWriter('output.mp4', fourcc, fps, (w, h))
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    results = model(frame, device = "mps")
-    result = results[0]
-    annotated_frame = results[0].plot()
-    bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
-    classes = np.array(result.boxes.cls.cpu(), dtype="int")
-    for cls, bbox in zip(classes, bboxes):
-        (x, y, x2, y2) = bbox
-        print(bbox)
-
-    cv2.imshow("Img", annotated_frame)
-    #out.write(annotated_frame)
-    if cv2.waitKey(1) == 27:
-        break
-
-
-cap.release()
-cv2.destroyAllWindows()
+# from list of PIL/ndarray
+results = model.predict(source=[im2])
